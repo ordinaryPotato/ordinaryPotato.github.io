31a77aa9ee28dcaff416aba0a9dc7083fa6251d2
Rob Hirst <9amindallas@gmail.com>
2016-12-13 19:19:06 -0800

    Delete thp.js

--- thp.js
+++
@@ -1,8 +0,0 @@
-$(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 200) {
-         $('#scroll-down').hide();
-     }
-     else {
-         $('#scroll-down').show();
-     }
-});
